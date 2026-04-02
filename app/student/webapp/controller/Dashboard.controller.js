d0628dbc0b0073047f6568717354d9228603d368
Pushpavalli P <pushpavalli@inflexiontechfze.com>
2026-04-02 05:28:43 +0000

    Dashbord

---
+++ app/student/webapp/controller/Dashboard.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+
+    return Controller.extend("webapp.student.controller.Dashboard", {
+        onInit(){},
+
+        onStudentsTilePress: function () {
+            const oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("StudentsList", { layout: "TwoColumnsMidExpanded" });
+        }
+
+    });
+});
